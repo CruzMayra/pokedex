b4c9704fce37e024de7e4487492faaa928ccefbf
CruzMayra <velazquez.leonora@gmail.com>
2018-03-04 14:14:38 -0600

    creación de función que pinta en modal datos del pokémon

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const $filterInput = $('.filtered-pokemon');
 
 $filterInput.keyup(filterPokemons);
 
+// función que busca la data y la 'cachea'
 if(!localStorage.getItem('data')) {
   $.post({
     url: 'https://graphql-pokemon.now.sh/',
@@ -15,6 +16,7 @@ if(!localStorage.getItem('data')) {
   getPokemonsData(localStorage.getItem('data'))
 }
 
+// función que obtiene la data 'cacheada'
 function getPokemonsData(data) {
   let pokemonsData = JSON.parse(data)
   .forEach(pokemon => {
@@ -22,14 +24,15 @@ function getPokemonsData(data) {
   })
 }
 
+// función que pinta en html cada pokemon
 function paintPokemonCard(pokemon){
   let card = '';
   card +=
-  `<section class="col-lg-3 col-sm-3 pokemon-card">
+  `<section class="col-lg-3 col-8 pokemon-card" data-toggle="modal" data-target="#pokemon-detail" data-id="${pokemon.id}">
     <div class="card">
-      <img class="card-img-top img-fluid" src="${pokemon.image}" alt="pokemon-${pokemon.name}">
+      <img class="card-img-top img-fluid rounded mx-auto d-block" src="${pokemon.image}" alt="pokemon-${pokemon.name}">
       <div class="card-body">
-        <h5 class="text-center" data-id="${pokemon.id}">${pokemon.name}</h5>
+        <h2 class="text-center">${pokemon.name}</h2>
       </div>
     </div>
   </section>`
@@ -37,6 +40,7 @@ function paintPokemonCard(pokemon){
   $('#pokemons-container').append(card)
 }
 
+// función que filtra los pokemones
 function filterPokemons(){
   let searchPokemon = $filterInput.val().toLowerCase();
   $('#pokemons-container').empty();
@@ -54,4 +58,48 @@ function filterPokemons(){
       paintPokemonCard(pokemon)
     })
   }
+}
+
+// función que crea los modales con los datos particulares del pokemon
+$('#pokemon-detail').on('show.bs.modal', function (event) {
+  let card = $(event.relatedTarget) // Button that triggered the modal
+  let pokemon = JSON.parse(localStorage.getItem('data')).find( pokemon => {
+    return pokemon.id === card.data('id')
+  })
+  var modal = $(this)
+  modal.find('.modal-title').text(pokemon.name);
+  modal.find('.modal-img').attr('src',pokemon.image);
+  modal.find('.classification').text(pokemon.classification);
+  modal.find('.type').empty().text(createTypesButtons(pokemon.types));
+  modal.find('.height').text(pokemon.height.maximum);
+  modal.find('.weight').text(pokemon.weight.maximum);
+  modal.find('.resistant').empty().text(createResistantButtons(pokemon.resistant));
+  modal.find('.weaknesses').empty().text(createWeaknessesButtons(pokemon.weaknesses));
+})
+
+// función que crea botones 'type' del modal
+function createTypesButtons(array){
+  return array.forEach(item => {
+    var templateButton = '';
+    templateButton += `<button type="button" class="btn btn-sm ${item}">${item}</button>`
+    $('.type').append(templateButton)
+  })
+}
+
+// función qye crea botoenes 'resistant' del modal
+function createResistantButtons(array){
+    return array.forEach(item => {
+      var templateButton = '';
+      templateButton += `<button type="button" class="btn btn-sm ${item}">${item}</button>`
+      $('.resistant').append(templateButton)
+    })
+}
+
+// función que crea los botones 'weaknesses' del modal
+function createWeaknessesButtons(array){
+    return array.forEach(item => {
+      var templateButton = '';
+      templateButton += `<button type="button" class="btn btn-sm ${item}">${item}</button>`
+      $('.weaknesses').append(templateButton)
+    })
 }
