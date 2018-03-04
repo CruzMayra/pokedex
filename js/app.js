d6d99662e3b34e679993728b74014598d6fb4964
CruzMayra <velazquez.leonora@gmail.com>
2018-03-04 01:05:56 -0600

    creación de función para filtro

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+const $filterInput = $('.filtered-pokemon');
+
+$filterInput.keyup(filterPokemons);
+
 if(!localStorage.getItem('data')) {
   $.post({
     url: 'https://graphql-pokemon.now.sh/',
@@ -19,17 +23,35 @@ function getPokemonsData(data) {
 }
 
 function paintPokemonCard(pokemon){
-  // $('#pokemons-container').empty();
   let card = '';
   card +=
-  `<section class="col-lg-3 pokemon-card">
+  `<section class="col-lg-3 col-sm-3 pokemon-card">
     <div class="card">
       <img class="card-img-top img-fluid" src="${pokemon.image}" alt="pokemon-${pokemon.name}">
       <div class="card-body">
-        <h4 class="text-center" data-id="${pokemon.id}">${pokemon.name}</h4>
+        <h5 class="text-center" data-id="${pokemon.id}">${pokemon.name}</h5>
       </div>
     </div>
   </section>`
 
   $('#pokemons-container').append(card)
+}
+
+function filterPokemons(){
+  let searchPokemon = $filterInput.val().toLowerCase();
+  $('#pokemons-container').empty();
+  if($filterInput.val().trim().length > 0){
+    var filteredPokemons = JSON.parse(localStorage.getItem('data')).filter( pokemon => {
+      let nameMatch = pokemon.name.toLowerCase().indexOf(searchPokemon) >=0
+      return nameMatch
+    }).forEach(pokemon => {
+      paintPokemonCard(pokemon)
+    })
+    $('#pokemons-container:empty').html('<p class="h1">Lo sentimos, no encontramos coincidencias <i class="fa fa-frown-o" aria-hidden="true"></i></p>');
+  } else {
+    $('#pokemons-container').empty();
+    JSON.parse(localStorage.getItem('data')).forEach(pokemon => {
+      paintPokemonCard(pokemon)
+    })
+  }
 }
