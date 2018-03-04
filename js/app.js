5cd4f86fcc66409416b5fab5dd70cc3bf419d494
CruzMayra <velazquez.leonora@gmail.com>
2018-03-04 00:17:14 -0600

    pintando tarjetas con todos los pokemones al cargar la página

--- js/app.js
+++ js/app.js
@@ -1,17 +1,35 @@
-$.ajax({
-    url: `https://pokeapi.co/api/v2/pokedex/1/`
-}).done(getPokemonId);
+if(!localStorage.getItem('data')) {
+  $.post({
+    url: 'https://graphql-pokemon.now.sh/',
+    data: JSON.stringify({ "query": " { pokemons(first: 151) { id name image height { maximum } weight { maximum } classification types resistant weaknesses } } " }),
+    contentType: 'application/json'
+  }).done(function(response) {
+    let pokemonsData = ('Fetched Pokemons:', response.data.pokemons);
+    localStorage.setItem('data', JSON.stringify(pokemonsData));
+  });
+} else {
+  getPokemonsData(localStorage.getItem('data'))
+}
 
-function getPokemonId(data) {
-  let pokemonCollection = data.pokemon_entries;
-  let method = pokemonCollection.forEach(item =>{
-    let pokemonId = item.entry_number;
-    $.ajax({
-      url: `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
-    }).done(getPokemonData);
+function getPokemonsData(data) {
+  let pokemonsData = JSON.parse(data)
+  .forEach(pokemon => {
+    paintPokemonCard(pokemon);
   })
 }
 
-function getPokemonData(data){
-  console.log(JSON.stringify(data));
+function paintPokemonCard(pokemon){
+  // $('#pokemons-container').empty();
+  let card = '';
+  card +=
+  `<section class="col-lg-3 pokemon-card">
+    <div class="card">
+      <img class="card-img-top img-fluid" src="${pokemon.image}" alt="pokemon-${pokemon.name}">
+      <div class="card-body">
+        <h4 class="text-center" data-id="${pokemon.id}">${pokemon.name}</h4>
+      </div>
+    </div>
+  </section>`
+
+  $('#pokemons-container').append(card)
 }
